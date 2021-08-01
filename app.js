657896abb84f551afb7f9d77a172898a3f581b9a
Answer-Lin <iamagod1253@gmail.com>
2021-08-01 17:55:49 +0800

    js

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
+const elDisplayName = document.getElementById("displayName")
+
 liff.init({
     liffId: "1656239633-vpX4nlk6" // Use own liffId
 }).then(() => {
     // Start to use liff's api
-    if (!liff.isLoggedIn()){
-        liff.login();
-        return;
-    }
-    console.log("LIFF loggedin");
-
     //Get LIFF User Profile
     liff.getProfile().then(profile =>{
-        document.getElementById('displayName').innerHTML = profile.displayName;
+        elDisplayName.innerHTML = profile.displayName;
     }).catch(err => {
         // if an error occurs
     });
