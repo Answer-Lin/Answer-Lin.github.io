6f81495bbaeb4afb153a5d050fc64c1b8f101794
Answer-Lin <iamagod1253@gmail.com>
2021-08-01 17:59:40 +0800

    js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const elDisplayName = document.getElementById("displayName")
-
 liff.init({
     liffId: "1656239633-vpX4nlk6" // Use own liffId
 }).then(() => {
     // Start to use liff's api
     //Get LIFF User Profile
     liff.getProfile().then(profile =>{
-        elDisplayName.innerHTML = profile.displayName;
+        document.getElementById('displayName').innerHTML = profile.displayName;
     }).catch(err => {
         // if an error occurs
     });
